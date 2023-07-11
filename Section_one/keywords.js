392a9d8a53d611ff7510e37acdba037c60c79659
Adarsh01208 <adarshsingh01209@gmail.com>
2023-07-11 17:20:40 +0530

    ..

--- Section_one/keywords.js
+++ Section_one/keywords.js
@@ -1,16 +1,28 @@
-age=21;
-if(age>=18)
-{
-   
+age = 21;
+if (age >= 18) {
     console.log('eligible');
-
-   
-    var y='funtion speed'
-
+    var y = 'funtion speed'
 }
-else
-{
+else {
     console.log('not eligible');
 }
 
 console.log(y);
+
+// palindrome pgm
+let no = 2002;
+let rev = 0;
+let temp = no;
+while (no > 0) {
+    let rem = no % 10;
+    rev = rev * 10 + rem;
+    no = parseInt(no / 10);
+}
+console.log(rev);
+
+if (no === rev) {
+    console.log("palindrome no")
+}
+else {
+    console.log("not Palindrome ")
+}
