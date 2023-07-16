350125c6e907c1a6c91f6b5ad99d6f89c3ab21d0
Adarsh01208 <adarshsingh01209@gmail.com>
2023-07-16 14:01:32 +0530

    add nav bar conver html to jsx format

--- Section_two/frontend/src/App.jsx
+++ Section_two/frontend/src/App.jsx
@@ -4,15 +4,18 @@ import Home from './components/Home';
 import Login from './components/Login';
 import Signin from './components/Signin';
 import Signup from './components/Signup';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-        <Link to="/home">Home Page</Link>
+        {/* <Link to="/home">Home Page</Link>
         <Link to="/login">Login Page</Link>
         <Link to="/signup"> Signup Page</Link>
-        <Link to="/signin">Signin Page</Link>
+        <Link to="/signin">Signin Page</Link> */}
+
+        <Navbar/>
 
         <Routes>
           <Route path='/' element={<Navigate to="/home" />} />
