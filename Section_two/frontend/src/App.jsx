100f275e2bf6c460fc0b634b620cb706fa2298bd
Adarsh01208 <adarshsingh01209@gmail.com>
2023-07-16 13:43:00 +0530

    ..

--- Section_two/frontend/src/App.jsx
+++ Section_two/frontend/src/App.jsx
@@ -1,4 +1,4 @@
-
+import { BrowserRouter, Routes, Route, Navigate, Link } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
 import Login from './components/Login';
@@ -8,10 +8,20 @@ import Signup from './components/Signup';
 function App() {
   return (
     <div>
-       <Home></Home>
-       <Signup></Signup>
-       <Login></Login>
-       <Signin></Signin>
+      <BrowserRouter>
+        <Link to="/home">Home Page</Link>
+        <Link to="/login">Login Page</Link>
+        <Link to="/signup"> Signup Page</Link>
+        <Link to="/signin">Signin Page</Link>
+
+        <Routes>
+          <Route path='/' element={<Navigate to="/home" />} />
+          <Route path='home' element={<Home />} />
+          <Route path='login' element={<Login />} />
+          <Route path='signin' element={<Signin />} />
+          <Route path='signup' element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- Section_two/frontend/src/components/Home.jsx
+++ Section_two/frontend/src/components/Home.jsx
@@ -4,6 +4,11 @@ const Home = () => {
   return (
     <div>
         <h1>Homepage</h1>
+        <p style={{fontSize : '50px' , backgroundColor: 'lightblue'}}>Inline Css in Jsx</p>
+        <p className='myclass'>We use className here in JSX</p>
+        <input type="text" />
+        <br />
+        <hr />
     </div>
   )
 }
