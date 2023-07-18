32522792593243400aeaa7c8cc546bc982f64032
Adarsh01208 <adarshsingh01209@gmail.com>
2023-07-18 14:06:45 +0530

    display img

--- Section_two/frontend/src/components/StateManagement.jsx
+++ Section_two/frontend/src/components/StateManagement.jsx
@@ -1,12 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const StateManagement = () => {
+
+    const [likes, setLikes] = useState(0);
+
+    const [imgUrl, setImgUrl] = useState('');
+
+    
+
+
     let count = 10;
+
+    
+    const selectFile = (e) => {
+        const file=e.target.files[0];
+        console.log(file);
+        //reading file
+        const reader =new FileReader(file);
+
+        reader.onload= (data) => {
+            console.log(data.target.result);
+            setImgUrl(data.target.result);
+        } ;
+
+        reader.readAsDataURL(file);
+
+    }
+
     return (
         <div>
             <div className='container'>
                 <h1>Count :  {count} </h1>
-                <button onClick={ () => { count++; console.log(count); } } > Add Count </button>
+                <button onClick={() => { count++; console.log(count); }} > Add Count </button>
+
+                <h1>Likes : {likes} </h1>
+                <button className='btn btn-primary' onClick={() => { setLikes(likes + 1) }}> Add Likes</button>
+                <button className='btn btn-danger' onClick={() => { setLikes(likes - 1) }}> DisLikes</button>
+            
+                <input type="file" onChange={selectFile} />
+
+                <img className="m-auto d-block w-50" src={imgUrl} alt="" />
             </div>
         </div>
     )
