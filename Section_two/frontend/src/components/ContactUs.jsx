b561286603d96074b05e49db1db7019c40661edf
Adarsh01208 <adarshsingh01209@gmail.com>
2023-08-02 18:49:57 +0530

    ..

--- Section_two/frontend/src/components/ContactUs.jsx
+++ Section_two/frontend/src/components/ContactUs.jsx
@@ -1,57 +1,55 @@
 import React from 'react'
-
-
+import { useFormik } from 'formik'
 
 const ContactUs = () => {
-    return (
-        <div>
-            <div className="container">
-                <div className="text">Contact us Form</div>
-                <form action="#">
-                    <div className="form-row">
-                        <div className="input-data">
-                            <input type="text" required="" />
-                            <div className="underline" />
-                            <label htmlFor="">First Name</label>
-                        </div>
-                        <div className="input-data">
-                            <input type="text" required="" />
-                            <div className="underline" />
-                            <label htmlFor="">Last Name</label>
-                        </div>
-                    </div>
-                    <div className="form-row">
-                        <div className="input-data">
-                            <input type="text" required="" />
-                            <div className="underline" />
-                            <label htmlFor="">Email Address</label>
-                        </div>
-                        <div className="input-data">
-                            <input type="text" required="" />
-                            <div className="underline" />
-                            <label htmlFor="">Phone No</label>
-                        </div>
-                    </div>
-                    <div className="form-row">
-                        <div className="input-data textarea">
-                            <textarea rows={8} cols={80} required="" defaultValue={""} />
-                            <br />
-                            <div className="underline" />
-                            <label htmlFor="">Write your message</label>
-                            <br />
-                            <div className="form-row submit-btn">
-                                <div className="input-data">
-                                    <div className="inner" />
-                                    <input type="submit" defaultValue="submit" />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </form>
+
+  const contactForm = useFormik(
+    {
+      initialValues: {
+        name: '',
+        email: '',
+        description: ''
+      },
+      onSubmit: async (values) => {
+        console.log(values);
+        //sending request  to backend
+        await fetch('http://localhost:5000/user/add', {
+          method: 'POST',
+          body: JSON.stringify(values),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        });
+        // console.log(res.status)
+      }
+    });
+
+  return (
+    <div className="d-flex justify-content-center align-items-center  vh-100 ">
+      <div className="card w-25 shadow-lg rounded-3   ">
+        <div className="card-body  px-4 py-3 ">
+          <i className="fa-solid fa-lock fa-3x d-block text-center " />
+          <h1 className="text-center my-4  ">Contact Form</h1>
+          <form onSubmit={contactForm.handleSubmit}>
+            <div>
+              <label htmlFor="">Name</label>
+              <input className="form-control mb-4 rounded-3" type="text" name="name" onChange={contactForm.handleChange} value={contactForm.values.name} />
             </div>
-
+            <div>
+              <label htmlFor="">Email</label>
+              <input className="form-control mb-4 rounded-3" type="email" name="email" onChange={contactForm.handleChange} value={contactForm.values.email} />
+            </div>
+            <div>
+              <label htmlFor="">Description</label>
+              <textarea className="form-control mb-4 rounded-3" rows={3} cols={30} required="" defaultValue={""} name="description" onChange={contactForm.handleChange} value={contactForm.values.description} />
+            </div>
+            <div>
+              <button type='submit' className="btn btn-danger w-100 mt-2 rounded-3 mb-4 ">Submit</button>
+            </div>
+          </form>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
-
 export default ContactUs
