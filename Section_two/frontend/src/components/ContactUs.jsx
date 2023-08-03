532caff865b44b16acf2527a92ee06acd6fac986
Adarsh01208 <adarshsingh01209@gmail.com>
2023-08-03 21:09:28 +0530

    add sweet alert

--- Section_two/frontend/src/components/ContactUs.jsx
+++ Section_two/frontend/src/components/ContactUs.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useFormik } from 'formik'
+import Swal from 'sweetalert2';
 
 const ContactUs = () => {
 
@@ -8,26 +9,42 @@ const ContactUs = () => {
       initialValues: {
         name: '',
         email: '',
-        description: ''
+        message: ''
       },
       onSubmit: async (values) => {
         console.log(values);
-        //sending request  to backend
-        await fetch('http://localhost:5000/user/add', {
+        //sending request  to backend   //required="" defaultValue={""}
+       const res= await fetch('http://localhost:5000/user/add', {
           method: 'POST',
           body: JSON.stringify(values),
           headers: {
             'Content-Type': 'application/json'
           }
         });
-        // console.log(res.status)
+          console.log(res.status)
+
+          if (res.status === 200) {
+            Swal.fire({
+              icon: 'success',
+              title: 'Thank You',
+              text: 'Team Will Contact U Shortly'
+            });
+          
+          }
+          else {
+            Swal.fire({
+              icon: 'error',
+              title: 'Oops!!',
+              text: 'Some Error Occured'
+            });
+          }
       }
     });
 
   return (
     <div className="d-flex justify-content-center align-items-center  vh-100 ">
-      <div className="card w-25 shadow-lg rounded-3   ">
-        <div className="card-body  px-4 py-3 ">
+      <div className="card w-25 shadow-lg rounded-5   ">
+        <div className="card-body p-5">
           <i className="fa-solid fa-lock fa-3x d-block text-center " />
           <h1 className="text-center my-4  ">Contact Form</h1>
           <form onSubmit={contactForm.handleSubmit}>
@@ -40,11 +57,12 @@ const ContactUs = () => {
               <input className="form-control mb-4 rounded-3" type="email" name="email" onChange={contactForm.handleChange} value={contactForm.values.email} />
             </div>
             <div>
-              <label htmlFor="">Description</label>
-              <textarea className="form-control mb-4 rounded-3" rows={3} cols={30} required="" defaultValue={""} name="description" onChange={contactForm.handleChange} value={contactForm.values.description} />
+              <label htmlFor="">Message</label>
+              <textarea className="form-control mb-4 rounded-3" rows={3} cols={30} type="String" name="message" onChange={contactForm.handleChange} value={contactForm.values.message} />
             </div>
+          
             <div>
-              <button type='submit' className="btn btn-danger w-100 mt-2 rounded-3 mb-4 ">Submit</button>
+              <button type='submit' className="btn btn-danger w-100 mt-2 rounded-3 ">Submit</button>
             </div>
           </form>
         </div>
@@ -52,4 +70,4 @@ const ContactUs = () => {
     </div>
   )
 }
-export default ContactUs
+export default ContactUs;
