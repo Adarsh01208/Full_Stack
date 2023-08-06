6694af512e78d2dfbd52e8ee304e184c93136065
Adarsh01208 <adarshsingh01209@gmail.com>
2023-08-06 12:47:03 +0530

    ..

--- Section_three/Backend/models/userModel.js
+++ Section_three/Backend/models/userModel.js
@@ -1,12 +1,12 @@
 const { model, Schema } = require('../Connection');
 
 const myschema = new Schema({
-    
+
     name: String,
     email: String,
     password: String,
     age: Number,
-    message:String
+    message: String
 
 });
 
