7485d29dcb16a9f3540d658e48aad5902a4d0676
Your Name <you@example.com>
2023-08-11 18:54:44 +0530

    ..

--- Section_two/frontend/src/components/UpdateUser.jsx
+++ Section_two/frontend/src/components/UpdateUser.jsx
@@ -1,4 +1,4 @@
-import { useFormik } from 'formik';
+import { Formik, useFormik } from 'formik';
 import React, { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom';
 import Swal from 'sweetalert2';
@@ -9,7 +9,7 @@ const UpdateUser = () => {
 
     const { id } = useParams();
 
-    const [userData, setuserData] = useState({});
+    const [userData, setuserData] = useState(null);
 
     const fetchUserData = async () => {
         const res = await fetch("http://localhost:5000/user/getid/" + id);
@@ -27,48 +27,6 @@ const UpdateUser = () => {
 
     }, []);
 
-    const signupForm = useFormik(
-        {
-            initialValues: {
-                name: '',
-                email: '',
-                password: ''
-            },
-            onSubmit: async (values) => {
-                console.log(values);
-
-                //sending request  to backend
-                const res = await fetch('http://localhost:5000/user/update/' + id, {
-                    method: 'PUT',
-                    body: JSON.stringify(values),
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
-                });
-                console.log(res.status)
-
-                if (res.status === 200) {
-                    Swal.fire({
-                        icon: 'success',
-                        title: 'Update Success',
-                        text: 'Now Login to continue'
-                    });
-
-                    navigate('/manageuser');
-
-                }
-                else {
-                    Swal.fire({
-                        icon: 'error',
-                        title: 'Oops!!',
-                        text: 'Some Error Occured'
-                    });
-                }
-
-            },
-
-        });
-
 
     return (
         <div>
@@ -78,26 +36,71 @@ const UpdateUser = () => {
                     <div className="card-body  p-5">
                         <i className="fa-solid fa-lock fa-3x d-block text-center " />
                         <h1 className="text-center my-4">Update Form</h1>
-                        <form onSubmit={signupForm.handleSubmit}>
-                            <div>
-                                <label htmlFor="">Name</label>
-                                <p className='error-label'>{signupForm.touched.name ? signupForm.errors.name : ''}</p>
-                                <input className="form-control mb-4 rounded-3" type="text" name="name" onChange={signupForm.handleChange} value={signupForm.values.name} />
-                            </div>
-                            <div>
-                                <label htmlFor="">Email</label>
-                                <p className='error-label'>{signupForm.touched.email ? signupForm.errors.email : ''}</p>
-                                <input className="form-control mb-4 rounded-3" type="email" name="email" onChange={signupForm.handleChange} value={signupForm.values.email} />
-                            </div>
-                            <div>
-                                <label htmlFor="">Password</label>
-                                <p className='error-label'>{signupForm.touched.password ? signupForm.errors.password : ''}</p>
-                                <input className="form-control mb-0 rounded-3" type="password" name="password" onChange={signupForm.handleChange} value={signupForm.values.password} />
-                            </div>
-                            <div>
-                                <button type='submit' className="btn btn-danger w-100 mt-4 rounded-3 ">Update User</button>
-                            </div>
-                        </form>
+                        {
+                            userData !== null ? (
+                                <Formik initialValues={userData}
+                                    onSubmit={async (values) => {
+                                        console.log(values);
+
+                                        //sending request  to backend
+                                        const res = await fetch('http://localhost:5000/user/update/' + id, {
+                                            method: 'PUT',
+                                            body: JSON.stringify(values),
+                                            headers: {
+                                                'Content-Type': 'application/json'
+                                            }
+                                        });
+                                        console.log(res.status)
+
+                                        if (res.status === 200) {
+                                            Swal.fire({
+                                                icon: 'success',
+                                                title: 'Update Success',
+                                                text: 'Now Login to continue'
+                                            });
+
+                                            navigate('/manageuser');
+
+                                        }
+                                        else {
+                                            Swal.fire({
+                                                icon: 'error',
+                                                title: 'Oops!!',
+                                                text: 'Some Error Occured'
+                                            });
+                                        }
+                                    }}>
+                                    {
+                                        ({ values, handleSubmit, handleChange, touched, errors }) => (
+
+                                            <form onSubmit={handleSubmit}>
+                                                <div>
+                                                    <label htmlFor="">Name</label>
+                                                    <p className='error-label'>{touched.name ? errors.name : ''}</p>
+                                                    <input className="form-control mb-4 rounded-3" type="text" name="name" onChange={handleChange} value={values.name} />
+                                                </div>
+                                                <div>
+                                                    <label htmlFor="">Email</label>
+                                                    <p className='error-label'>{touched.email ? errors.email : ''}</p>
+                                                    <input className="form-control mb-4 rounded-3" type="email" name="email" onChange={handleChange} value={values.email} />
+                                                </div>
+                                                <div>
+                                                    <label htmlFor="">Password</label>
+                                                    <p className='error-label'>{touched.password ? errors.password : ''}</p>
+                                                    <input className="form-control mb-0 rounded-3" type="password" name="password" onChange={handleChange} value={values.password} />
+                                                </div>
+
+                                                <div>
+                                                    <button type='submit' className="btn btn-danger w-100 mt-4 rounded-3 ">Update User</button>
+                                                </div>
+                                            </form>
+                                        )
+                                    }
+                                </Formik>
+                            ) :
+                                <h1>Loading ...</h1>
+                        }
+
                     </div>
                 </div>
             </div>
