f8b54b9d422e868e9304b9445d67ab6b3ed06d18
Your Name <adarshsingh01209@gmail.com>
2023-08-11 19:00:34 +0530

    ..

--- Section_two/frontend/src/App.jsx
+++ Section_two/frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate  } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
 import Login from './components/Login';
@@ -12,35 +12,40 @@ import ContactUs from './components/ContactUs';
 import Browse from './components/Browse';
 import ManageUser from './components/ManageUser';
 import { Toaster } from 'react-hot-toast';
+import UpdateUser from './components/UpdateUser';
+import { UserProvider } from './UserContext';
+import Profile from './components/Profile';
 
 
 
 function App() {
   return (
     <div>
-      <Toaster position= 'top-center' />
+      <Toaster position='top-center' />
 
       <BrowserRouter>
-        {/* <Link to="/home">Home Page</Link>
+        <UserProvider>
+          {/* <Link to="/home">Home Page</Link>
         <Link to="/login">Login Page</Link>
         <Link to="/signup"> Signup Page</Link>
         <Link to="/signin">Signin Page</Link> */}
-
-        <Navbar />
-
-        <Routes>
-          <Route path='/' element={<Navigate to="/home" />} />
-          <Route path='home' element={<Home />} />
-          <Route path='login' element={<Login />} />
-          <Route path='signup' element={<Signup />} />
-          <Route path='event' element={<EventHandling/>} />
-          <Route path='statemanagement' element={<StateManagement/>} />
-          <Route path='todo' element={<Todo/>} />
-          <Route path='chatapp' element={<ChatApp/>} />
-          <Route path='contactus' element={<ContactUs/>} />
-          <Route path='browse' element={<Browse/>} />
-          <Route path='manageuser' element={<ManageUser/>} />
-        </Routes>
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<Navigate to="/home" />} />
+            <Route path='home' element={<Home />} />
+            <Route path='login' element={<Login />} />
+            <Route path='signup' element={<Signup />} />
+            <Route path='event' element={<EventHandling />} />
+            <Route path='statemanagement' element={<StateManagement />} />
+            <Route path='todo' element={<Todo />} />
+            <Route path='chatapp' element={<ChatApp />} />
+            <Route path='contactus' element={<ContactUs />} />
+            <Route path='browse' element={<Browse />} />
+            <Route path='manageuser' element={<ManageUser />} />
+            <Route path='updateuser/:id' element={<UpdateUser />} />
+            <Route path='profile/:id' element={<Profile />} />
+          </Routes>
+        </UserProvider>
       </BrowserRouter>
     </div>
   );
